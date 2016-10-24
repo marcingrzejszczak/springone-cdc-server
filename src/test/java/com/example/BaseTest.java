565900f37a4c6bb0273a010c78e210e2b9c0abf7
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-10-24 18:05:42 +0200

    No implementation

--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -2,13 +2,11 @@ package com.example;
 
 import org.junit.Before;
 
-import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
-
 public class BaseTest {
 
 	@Before
 	public void setup() {
-		RestAssuredMockMvc.standaloneSetup(new ProducerController());
+
 	}
 	
 }
