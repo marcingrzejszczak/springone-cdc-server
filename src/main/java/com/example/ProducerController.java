c1a4afec0b231ee84e6b7f3cbcc3a3ba663a6a06
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-10-27 11:33:09 +0200

    Added interface

--- src/main/java/com/example/ProducerController.java
+++ src/main/java/com/example/ProducerController.java
@@ -8,17 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProducerController {
 
+	private final ProducerService producerService;
+
+	public ProducerController(ProducerService producerService) {
+		this.producerService = producerService;
+	}
+
 	@RequestMapping(value = "/check",
 			method=RequestMethod.POST,
 			consumes="application/json",
 			produces="application/json")
 	public Response check(@RequestBody PersonToCheck personToCheck) {
-		if (personToCheck.age >= 20) {
+		boolean canGetTheBeer = producerService.canGetTheBeer(personToCheck);
+		if (canGetTheBeer) {
 			return new Response(BeerCheckStatus.OK);
 		}
 		return new Response(BeerCheckStatus.NOT_OK);
 	}
 	
+}
+
+interface ProducerService {
+	boolean canGetTheBeer(PersonToCheck personToCheck);
 }
 
 class PersonToCheck {
--- src/test/java/com/example/BaseTest.java
+++ src/test/java/com/example/BaseTest.java
@@ -1,14 +1,47 @@
 package com.example;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Before;
+import org.mockito.Mockito;
 
 import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+
+import static org.mockito.Matchers.argThat;
+import static org.mockito.Mockito.when;
 
 public class BaseTest {
 
 	@Before
 	public void setup() {
-		RestAssuredMockMvc.standaloneSetup(new ProducerController());
+		ProducerService producerService = Mockito.mock(ProducerService.class);
+		when(producerService.canGetTheBeer(argThat(personIsOldEnough()))).thenReturn(true);
+		when(producerService.canGetTheBeer(argThat(personIsTooYoung()))).thenReturn(false);
+
+		RestAssuredMockMvc.standaloneSetup(new ProducerController(producerService));
 	}
-	
+
+	private TypeSafeMatcher<PersonToCheck> personIsOldEnough() {
+		return new TypeSafeMatcher<PersonToCheck>() {
+			@Override public void describeTo(Description description) {
+
+			}
+			@Override
+			protected boolean matchesSafely(PersonToCheck item) {
+				return item.age >= 20;
+			}
+		};
+	}
+	private TypeSafeMatcher<PersonToCheck> personIsTooYoung() {
+		return new TypeSafeMatcher<PersonToCheck>() {
+			@Override public void describeTo(Description description) {
+
+			}
+			@Override
+			protected boolean matchesSafely(PersonToCheck item) {
+				return item.age < 20;
+			}
+		};
+	}
+
 }
